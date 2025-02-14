429e45446a9313543a4a5ccd8bed119b13e6dde9
Yoshi Odei <yoshiodei@yahoo.com>
2025-02-14 09:28:16 +0000

    test publish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ const Home: React.FC = () => {
     <div className="flex items-center justify-center h-[80vh] bg-background">
       <div>
         <div className="flex justify-center w-full">
-          <img src="/images/nudiance_logo.png" alt="..." className="w-[100px]" />
+          <img src="/images/nudiance_logo.png" alt=".." className="w-[100px]" />
         </div>
         <h2 className="2xl:text-4xl xl:text-4xl text-xl text-slate-400 font-bold my-5 2xl:w-[45vw] xl:w-[45vw] w-full text-center">Log in to access personalized tools and resources tailored to your business needs.</h2>
         <div className="flex justify-center">
